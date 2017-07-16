7de410eda844bbfa1d919ecdfacb9c3d5177600b
Sasha Walsh <walsh.s443@gmail.com>
2017-07-16 16:46:39 -0400

    server stuff

--- index.js
+++ index.js
@@ -1,22 +1,28 @@
 const bodyParser = require('body-parser')
 const express = require('express')
 const mongoose = require('mongoose')
+const hbs = require('express-handlebars')
+
 
 mongoose.connect('mongodb://sashalou25:$a$haWa1$h@ds139322.mlab.com:39322/sasha-tunr')
 
 //require models (I don't think I have any yet?)
 
 //require controllers aka routers (I don't think I have any yet?)
-const appRoutes = require('./routes/index.js')
+//const appRoutes = require('./routes/index.js')
 
 //making out application
 const app = express()
+
+// Registering and use our template engine (handlebars)
+app.engine('handlebars', hbs({defaultLayout: 'main'}))
+app.set('view engine', 'handlebars')
 
 
 // Serving static files (like css)
 //app.use('/static', express.static('public'))
 
-app.use('/game',express.static('public'))
+app.use(express.static('public'))
 
  //app.use('/game', express.static(__dirname + '/public'));
 
---
+++ public/index.js
@@ -0,0 +1,96 @@
+const express = require('express')
+
+//I will need this later when I start building out the hall of fame
+//const Post = require('../models/posts.js')
+
+const appRouter = express.Router()
+
+//player selects "play game" on the Welcome page--> this happens on welcome.html
+
+
+//player is taken to the game page aka game-page.html
+
+
+//Upon game page loading "Ready Set Go!" displays on the screen for three seconds
+
+function dissapear(){
+document.getElementByID("msg")
+setTimeOut(3000)
+
+}
+
+//round 1 (3 seconds apart)
+
+//pop up number 1 should appear after three seconds
+
+  //if the user clicks the popup move on to next popup
+  //if not show "GAME OVER" page
+
+//pop up number 2 should appear after six seconds
+  //if the user clicks the popup move on to next popup
+  //if not show "GAME OVER" page
+
+//pop up number 3 should appear after nine seconds
+  //if the user clicks the popup move on to next popup
+  //if not show "GAME OVER" page
+
+//pop up number 4 should appear after twelve seconds
+  //if the user clicks the popup move on to next popup
+  //if not show "GAME OVER" page
+
+//pop up number 5 should appear after fifteen seconds
+
+
+
+//round 2 (2 seconds apart)
+
+//pop up number 6 should appear after 17 seconds
+  //if the user clicks the popup move on to next popup
+  //if not show "GAME OVER" page
+
+//pop up number 7 should appear after 19 seconds
+  //if the user clicks the popup move on to next popup
+  //if not show "GAME OVER" page
+
+//pop up number 8 should appear after 21 seconds
+  //if the user clicks the popup move on to next popup
+  //if not show "GAME OVER" page
+
+//pop up number 9 should appear after 23 seconds
+  //if the user clicks the popup move on to next popup
+  //if not show "GAME OVER" page
+
+//pop up number 10 should appear after 25 seconds
+  //if the user clicks the popup move on to next popup
+  //if not show "GAME OVER" page
+
+
+//round 3 (1 second apart)
+
+//pop up number 11 should appear after 26 seconds
+  //if the user clicks the popup move on to next popup
+  //if not show "GAME OVER" page
+
+//pop up number 12 should appear after 27 seconds
+  //if the user clicks the popup move on to next popup
+  //if not show "GAME OVER" page
+
+//pop up number 13 should appear after 28 seconds
+  //if the user clicks the popup move on to next popup
+  //if not show "GAME OVER" page
+
+//pop up number 14 should appear after 29 seconds
+  //if the user clicks the popup move on to next popup
+  //if not show "GAME OVER" page
+
+//pop up number 15 should appear after 30 seconds
+  //if the user clicks the popup move on to next popup
+  //if not show "GAME OVER" page
+
+//If user completes this round show "YOU WON" message which has option to play again or enter your name in the hall of fame
+
+
+
+//Real game logic =:0
+
+module.exports = postsRouter
